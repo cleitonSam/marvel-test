c36afb6dd7d04cdd56a238941afdfd5f055dc199
cleitonSam <cleiton.nso2019@gmail.com>
2023-02-18 20:30:37 -0300

    feat(footer): criando footer

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  tableContent = [
+    { id: 12, name: 'Dr. Nice' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+  ];
+}

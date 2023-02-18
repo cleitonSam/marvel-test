7f16bb63628593debd1bcb0f4cc6ad3b4e58a06b
cleitonSam <cleiton.nso2019@gmail.com>
2023-02-18 02:05:45 -0300

    feat(search): criando o componente de busca

---
+++ src/app/shared/marvel-search/marvel-search.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marvel-search',
+  templateUrl: './marvel-search.component.html',
+  styleUrls: ['./marvel-search.component.scss'],
+})
+export class MarvelSearchComponent {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MarvelHeaderComponent } from './marvel-header/marvel-header.component';
+import { MarvelSearchComponent } from './marvel-search/marvel-search.component';
 
 @NgModule({
-  declarations: [MarvelHeaderComponent],
+  declarations: [MarvelHeaderComponent, MarvelSearchComponent],
   imports: [CommonModule],
   exports: [MarvelHeaderComponent],
 })

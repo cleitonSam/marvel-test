fc941570636bc2bb4d4a1d57fb78be772b4840a1
cleitonSam <cleiton.nso2019@gmail.com>
2023-02-18 15:05:44 -0300

    feat(footer): criando footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MarvelHeaderComponent } from './marvel-header/marvel-header.component';
 import { MarvelSearchComponent } from './marvel-search/marvel-search.component';
+import { MarvelFooterComponent } from './marvel-footer/marvel-footer.component';
 
 @NgModule({
-  declarations: [MarvelHeaderComponent, MarvelSearchComponent],
+  declarations: [MarvelHeaderComponent, MarvelSearchComponent, MarvelFooterComponent],
   imports: [CommonModule],
-  exports: [MarvelHeaderComponent, MarvelSearchComponent],
+  exports: [MarvelHeaderComponent, MarvelSearchComponent, MarvelFooterComponent],
 })
 export class SharedModule {}
